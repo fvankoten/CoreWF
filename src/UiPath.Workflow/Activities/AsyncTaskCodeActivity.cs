08a370f8f73e8e8e1d0bfcd35b9ae92ca5d3d5a9
AlUllrich <ullrich@lcs-schlieben.de>
2026-02-25 09:33:56 +0100

    added Pre-/Postexecute functions to AsyncTaskCodeActivity

--- src/UiPath.Workflow/Activities/AsyncTaskCodeActivity.cs
+++ src/UiPath.Workflow/Activities/AsyncTaskCodeActivity.cs
@@ -4,64 +4,94 @@
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
+using UiPath.Workflow.Models;
 
 namespace System.Activities;
 
-public abstract class AsyncTaskCodeActivity : TaskCodeActivity<object>
+public abstract class AsyncTaskCodeActivity : TaskCodeActivity<object, object>
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Browsable(false)]
     public new object Result { get; set; }
 
-    protected abstract Task ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken);
+    protected abstract Task ExecuteAsync(AsyncCodeActivityContext context, object state, CancellationToken cancellationToken);
 
-    private protected sealed override async Task<object> ExecuteAsyncCore(AsyncCodeActivityContext context,
-        CancellationToken cancellationToken)
+    private protected sealed override async Task<object> ExecuteAsyncCore(AsyncCodeActivityContext context, object state, CancellationToken cancellationToken)
     {
-        await ExecuteAsync(context, cancellationToken);
+        await ExecuteAsync(context, state, cancellationToken);
         return null;
     }
 }
 
-public abstract class AsyncTaskCodeActivity<TResult> : TaskCodeActivity<TResult>
+public abstract class AsyncTaskCodeActivity<TResult> : TaskCodeActivity<TResult, object>
 {
-    protected abstract Task<TResult>
-        ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken);
-
-    private protected sealed override Task<TResult> ExecuteAsyncCore(AsyncCodeActivityContext context,
-        CancellationToken cancellationToken)
+    protected abstract Task<TResult> ExecuteAsync(AsyncCodeActivityContext context, object state, CancellationToken cancellationToken);
+    private protected sealed override Task<TResult> ExecuteAsyncCore(AsyncCodeActivityContext context, object state, CancellationToken cancellationToken)
     {
-        return ExecuteAsync(context, cancellationToken);
+        return ExecuteAsync(context, state, cancellationToken);
+    }
+}
+
+public abstract class AsyncTaskCodeActivity<TResult, TState> : TaskCodeActivity<TResult, TState> where TState : class
+{
+    protected abstract Task<TResult> ExecuteAsync(AsyncCodeActivityContext context, TState state, CancellationToken cancellationToken);
+
+    private protected sealed override Task<TResult> ExecuteAsyncCore(AsyncCodeActivityContext context, TState state, CancellationToken cancellationToken)
+    {
+        return ExecuteAsync(context, state, cancellationToken);
     }
 }
 
 [EditorBrowsable(EditorBrowsableState.Never)]
-public abstract class TaskCodeActivity<TResult> : AsyncCodeActivity<TResult>
+public abstract class TaskCodeActivity<TResult, TState> : AsyncCodeActivity<TResult> where TState : class
 {
-    protected sealed override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback,
-        object state)
+    protected sealed override IAsyncResult BeginExecute(AsyncCodeActivityContext context, AsyncCallback callback, object state)
     {
         var cts = new CancellationTokenSource();
-        context.UserState = cts;
 
-        var task = ExecuteAsyncCore(context, cts.Token);
+        var userState = PreExecute(context);
+
+        context.UserState = new ActivityState<TState>()
+        {
+            CancellationTokenSource = cts,   
+            State = userState
+        };
+
+        var task = ExecuteAsyncCore(context, userState, cts.Token);
 
         return TaskToAsyncResult.Begin(task, callback, state);
     }
 
     protected sealed override TResult EndExecute(AsyncCodeActivityContext context, IAsyncResult result)
     {
-        using ((CancellationTokenSource)context.UserState)
+        var activityState = (ActivityState<TState>)context.UserState;
+
+        using (activityState.CancellationTokenSource)
         {
-            return TaskToAsyncResult.End<TResult>(result);
+            var taskResult = TaskToAsyncResult.End<TResult>(result);
+
+            var activityResult = PostExecute(context, activityState.State, taskResult);
+
+            return activityResult;
         }
+    }
+
+    protected virtual TState PreExecute(AsyncCodeActivityContext context)
+    {
+        return null;
+    }
+
+    protected virtual TResult PostExecute(AsyncCodeActivityContext context, TState activityState, TResult result)
+    {
+        return result;
     }
 
     protected sealed override void Cancel(AsyncCodeActivityContext context)
     {
-        ((CancellationTokenSource)context.UserState).Cancel();
+        var activityState = (ActivityState<TState>)context.UserState;
+
+        activityState.CancellationTokenSource.Cancel();
     }
 
-    private protected abstract Task<TResult> ExecuteAsyncCore(AsyncCodeActivityContext context,
-        CancellationToken cancellationToken);
+    private protected abstract Task<TResult> ExecuteAsyncCore(AsyncCodeActivityContext context, TState? state, CancellationToken cancellationToken);
 }
---
+++ src/UiPath.Workflow/Models/ActivityState.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+
+namespace UiPath.Workflow.Models;
+
+public sealed class ActivityState<TState> where TState : class
+{
+    public CancellationTokenSource CancellationTokenSource { get; init; }
+    public TState State { get; set; }
+}
